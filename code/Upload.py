59834e354fddfa14b34983abfe693260dc15f33f
Aaryan <pes1202102130@pesu.pes.edu>
2024-08-04 20:29:11 +0530

    Updated Upload.py in order to upload vectors into namespaces

--- code/Upload.py
+++ code/Upload.py
@@ -4,26 +4,25 @@ from tqdm.auto import tqdm
 from typing import List
 import pinecone
 import langchain
-from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.docstore.document import Document
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def read_and_filter_doc(directory, skip_pages=6):
-    file_loader = PyPDFDirectoryLoader(directory)
+
+def read_and_filter_doc(file_path):    
+    file_loader = PyPDFLoader(file_path)
     documents = file_loader.load()
-    filtered_documents = [doc for i, doc in enumerate(documents) if i >= skip_pages]
-    return filtered_documents
+   
+    if file_path == '../pdf/trp_act.pdf':
+        filtered_documents = [doc for i, doc in enumerate(documents) if i >= 6]
+        documents = filtered_documents
+    return documents
 
-doc = read_and_filter_doc('../pdf/')
-print("LENGTH", len(doc))
-
-def chunk_data(docs, chunk_size=1000, chunk_overlap=70):
+def chunk_data(docs, chunk_size=800, chunk_overlap=50):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     coherent_documents = []
     for document in docs:
@@ -31,32 +30,39 @@ def chunk_data(docs, chunk_size=1000, chunk_overlap=70):
         coherent_documents.extend(chunks)
     return coherent_documents
 
-documents = chunk_data(docs=doc)
-print("Documents Length", len(documents))
-
-api_key = os.getenv('GOOGLE_API_KEY')
-embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", api_key=api_key)
-print("Embedding", embeddings)
-
-pinecone_client = pinecone.Pinecone(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENVIRONMENT"))
-pc = pinecone.Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-index = pc.Index("check1")
-
 def docs_to_vectors(docs: List[Document], embeddings: GoogleGenerativeAIEmbeddings):
     vectors = []
     for doc in tqdm(docs):
         embedding = embeddings.embed_query(doc.page_content)
         metadata = doc.metadata
         metadata['text'] = doc.page_content
-        vectors.append((str(uuid.uuid4()), embedding, metadata))
+        vectors.append({"id": str(uuid.uuid4()), "values": embedding, "metadata": metadata})
     return vectors
 
-def upload_to_pinecone(vectors, index):
-    batch_size = 100
-    for i in tqdm(range(0, len(vectors), batch_size)):
-        batch = vectors[i:i+batch_size]
-        index.upsert(vectors=batch)
-    print(f"Uploaded {len(vectors)} vectors to Pinecone index")
 
-vectors = docs_to_vectors(documents, embeddings)
-upload_to_pinecone(vectors, index)
+def process_and_upload(directory):
+    api_key = os.getenv('GOOGLE_API_KEY')
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", api_key=api_key)
+    pinecone_client = pinecone.Pinecone(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENVIRONMENT"))
+    
+    for filename in os.listdir(directory):
+        
+        namespace = filename[:-4]  
+        pinecone_client = pinecone.Pinecone(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENVIRONMENT"))
+        pc = pinecone.Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+        index = pc.Index("check1")
+        print("-"*80)
+            
+        doc_path = os.path.join(directory, filename)
+        print(f"Processing and uploading {doc_path}")
+        doc = read_and_filter_doc(doc_path)
+        print(f"Read {len(doc)} documents from {doc_path}")
+        documents = chunk_data(docs=doc)
+        print(f"Split documents into {len(documents)} chunks")
+        vectors = docs_to_vectors(documents, embeddings)
+         
+            
+        index.upsert(vectors=vectors, namespace=namespace)
+        print(f"Uploaded {len(vectors)} vectors to Pinecone namespace {namespace}")
+
+process_and_upload('../pdf/')
