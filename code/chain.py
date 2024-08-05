e31204f3a2b79a5f82b373d2824c70052920c3f3
Aaryan <pes1202102130@pesu.pes.edu>
2024-08-05 17:15:10 +0530

    Updated chain.py in order to handle namespaces

--- code/chain.py
+++ code/chain.py
@@ -7,14 +7,15 @@ from dotenv import load_dotenv
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 load_dotenv()
 
+
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-vectorstore = LangchainPinecone.from_existing_index(index_name="check1", embedding=embeddings)
+def init_vectorstore(namespace):
+    return LangchainPinecone.from_existing_index(index_name="check1", embedding=embeddings, namespace=namespace)
 
 
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature=0.5)
-
 
 prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
@@ -27,23 +28,27 @@ PROMPT = PromptTemplate(
 )
 
 
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=vectorstore.as_retriever(),
-    return_source_documents=True,
-    chain_type_kwargs={"prompt": PROMPT}
-)
+def setup_qa_chain(namespace):
+    vectorstore = init_vectorstore(namespace)
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff", 
+        retriever=vectorstore.as_retriever(),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": PROMPT}
+    )
+    return qa_chain
 
 
-def query_rag(question):
+def query_rag(question, namespace):
+    qa_chain = setup_qa_chain(namespace)
     result = qa_chain({"query": question})
     
-    # Extract the answer and source documents
+   
     answer = result["result"]
     sources = result["source_documents"]
     
-    # Print the context (retrieved documents)
+    
     print("Context used for generating the answer:")
     for i, doc in enumerate(sources):
         print(f"Context {i+1}:")
@@ -52,11 +57,10 @@ def query_rag(question):
     
     return answer, sources
 
-question = "Can you talk about  Condition restraining alienation"
-answer, sources = query_rag(question)
-print(f"Question: {question}")
-print(f"Answer: {answer}")
-print("\nSources:")
-#for i, doc in enumerate(sources):
-    #print(f"Source {i+1}: {doc.metadata.get('source', 'Unknown')}")
 
+if __name__ == "__main__":
+    question = "According to the transfer of property act can you talk about what may be transferred?"
+    namespace = "The_Indian_Contract_Act_1872"
+    answer, sources = query_rag(question, namespace)
+    print(f"Question: {question}")
+    print(f"Answer: {answer}")
